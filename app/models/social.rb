5928f45850b75828edd232a78c70d3a3497e6c3f
thomas rodier <rodiert17@gmail.com>
2020-07-20 16:03:24 +0200

    update validation on socials

--- app/models/social.rb
+++ app/models/social.rb
@@ -2,5 +2,5 @@ class Social < ApplicationRecord
   has_many :networks
   has_many :profiles, through: :networks
 
-  validates :name, :url_desktop_begin, :url_mobile_begin, presence: true;
+  validates :name, presence: true;
 end
