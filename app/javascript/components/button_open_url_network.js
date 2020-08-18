14cb6c643529ae0af39bd87a7000c16a202477bb
thomas rodier <rodiert17@gmail.com>
2020-08-18 20:14:11 +0200

    update for good feature : Now you can test the link that you just enter

--- app/javascript/components/button_open_url_network.js
+++ app/javascript/components/button_open_url_network.js
@@ -2,18 +2,27 @@ const modalOpenNetwork = () => {
   if (document.getElementById('show-profil')) {
     const inputToListen = document.querySelectorAll(".js-listen-input");
 
+    function keyUpOrCopyPast(input, timer) {
+      if (timer) {
+          window.clearTimeout(timer);
+      }
+      timer = window.setTimeout( function() {
+          timer = null;
+          const buttonToChange = input.parentNode.parentNode.parentNode.querySelector(".modal-footer > a.button-to-open");
+          const urlToRedirect = buttonToChange.dataset.url;
+          buttonToChange.href=`${urlToRedirect + input.value}`
+          console.log(buttonToChange)
+      }, 50 );
+    }
+
     inputToListen.forEach((input) => {
       console.log(input);
       var timer = null;
       input.addEventListener('keyup', (e) => {
-        if (timer) {
-            window.clearTimeout(timer);
-        }
-        timer = window.setTimeout( function() {
-            timer = null;
-            const buttonToChange = input.parentNode.parentNode.parentNode.querySelector(".modal-footer > a.button-to-open");
-            buttonToChange.href=`${input.value}`
-        }, 100 );
+        keyUpOrCopyPast(input, timer)
+      })
+      input.addEventListener('copy', (e) => {
+        keyUpOrCopyPast(input, timer)
       })
     })
 
